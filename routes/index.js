60edad8e19deb4af7ce02a94b2cf70780e52a782
Bradley Jensen <bradley13@gmail.com>
2019-05-06 21:05:46 -0400

    start scripts fixed, created a test route to the back end

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/express-backend', function(req, res, next) {
+  res.send({
+    express: "YOUR BACK END IS CONNECTED...AND HOT"
+  });
 });
 
 module.exports = router;
