0a4c476214ba9b8a780b6ccc0ef5666d33c03f85
Bradley Jensen <bradley13@gmail.com>
2019-05-11 12:18:06 -0400

    removed / in front of client

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api', apiRouter);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // catch 404 and forward to error handler
