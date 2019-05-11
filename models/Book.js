f0d100aa1f2afaa68b3c90f3f7181a1d96c1d267
Bradley Jensen <bradley13@gmail.com>
2019-05-11 10:04:17 -0400

    added mongoose and bookSchema

---
+++ models/Book.js
@@ -0,0 +1,162 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+	kind: {
+		type: 'String',
+	},
+	id: {
+		type: 'String',
+		unique: true,
+	},
+	etag: {
+		type: 'String',
+	},
+	selfLink: {
+		type: 'String',
+	},
+	volumeInfo: {
+		title: {
+			type: 'String',
+		},
+		subtitle: {
+			type: 'String',
+		},
+		authors: {
+			type: ['String'],
+		},
+		publisher: {
+			type: 'String',
+		},
+		publishedDate: {
+			type: 'Date',
+		},
+		description: {
+			type: 'String',
+		},
+		industryIdentifiers: {
+			type: ['Mixed'],
+		},
+		readingModes: {
+			text: {
+				type: 'Boolean',
+			},
+			image: {
+				type: 'Boolean',
+			},
+		},
+		pageCount: {
+			type: 'Number',
+		},
+		printedPageCount: {
+			type: 'Number',
+		},
+		printType: {
+			type: 'String',
+		},
+		categories: {
+			type: ['String'],
+		},
+		averageRating: {
+			type: 'Number',
+		},
+		ratingsCount: {
+			type: 'Number',
+		},
+		maturityRating: {
+			type: 'String',
+		},
+		allowAnonLogging: {
+			type: 'Boolean',
+		},
+		contentVersion: {
+			type: 'String',
+		},
+		panelizationSummary: {
+			containsEpubBubbles: {
+				type: 'Boolean',
+			},
+			containsImageBubbles: {
+				type: 'Boolean',
+			},
+		},
+		imageLinks: {
+			smallThumbnail: {
+				type: 'String',
+			},
+			thumbnail: {
+				type: 'String',
+			},
+		},
+		language: {
+			type: 'String',
+		},
+		previewLink: {
+			type: 'String',
+		},
+		infoLink: {
+			type: 'String',
+		},
+		canonicalVolumeLink: {
+			type: 'String',
+		},
+	},
+	layerInfo: {
+		layers: {
+			type: ['Mixed'],
+		},
+	},
+	saleInfo: {
+		country: {
+			type: 'String',
+		},
+		saleability: {
+			type: 'String',
+		},
+		isEbook: {
+			type: 'Boolean',
+		},
+	},
+	accessInfo: {
+		country: {
+			type: 'String',
+		},
+		viewability: {
+			type: 'String',
+		},
+		embeddable: {
+			type: 'Boolean',
+		},
+		publicDomain: {
+			type: 'Boolean',
+		},
+		textToSpeechPermission: {
+			type: 'String',
+		},
+		epub: {
+			isAvailable: {
+				type: 'Boolean',
+			},
+		},
+		pdf: {
+			isAvailable: {
+				type: 'Boolean',
+			},
+		},
+		webReaderLink: {
+			type: 'String',
+		},
+		accessViewStatus: {
+			type: 'String',
+		},
+		quoteSharingAllowed: {
+			type: 'Boolean',
+		},
+	},
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model('Book', bookSchema);
+
+// Export the Article model
+module.exports = Book;
